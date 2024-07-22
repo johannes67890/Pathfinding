2425107dd8364b8b6b56502764df0a798a2d2c06
Caniuss <johannes@orager.dk>
2024-07-22 23:09:45 +0200

    refactor: Update React Router and dependencies to latest versions

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,47 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 import Grid from "./components/grid/Grid";
 import {  HeaderGraph, HeaderGrid } from "./components/Headers";
 import GridContexts from "./components/grid/context/GridContexts";
 import Renderer from "./components/Threejs/Renderer";
+import { Route, Routes, ErrorResponse, Navigate } from "react-router";
+import Root from "./components/Root";
+import Error from "./components/Error";
+import NotFound from "./components/NotFound";
 const container = document.getElementById("root");
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
 
 const Index = () => {
   return (
     <React.StrictMode>
-      <div className="max-w-7xl mx-auto mt-3">
-        {/* <GridContexts>
-          <HeaderGrid />
-          <Grid />
-        </GridContexts> */}
-        <HeaderGraph />
-        <Renderer  />
-      </div>
+      <BrowserRouter>
+        <div className="max-w-7xl mx-auto mt-3">
+          <Routes>
+            <Route path="/" element={<Root />} errorElement={<Error />} />
+
+            <Route path="/grid" element={
+              <GridContexts>
+                <HeaderGrid />
+                <Grid />
+              </GridContexts>
+            } 
+            errorElement={<Error />}
+            />
+            
+
+            <Route path="/graph" element={
+              <>
+                <HeaderGraph />
+                <Renderer  />
+              </>
+            } />
+          <Route path="*" element={<NotFound />} />
+          {/* <Route path="/grid/*" element={<Navigate to="/grid" replace />} /> */}
+          </Routes>
+        </div>
+      </BrowserRouter>
     </React.StrictMode>
   );
 };
